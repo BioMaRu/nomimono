cb29f3750e20a03ba1668ec53bb2a48cb3242872
BioMaRu <Bio.color.palette@gmail.com>
2022-08-15 15:49:53 +0700

    feat: refactor scss src

--- packages/nomimono-css/Gulpfile.js
+++ packages/nomimono-css/Gulpfile.js
@@ -13,7 +13,7 @@ gulp.task('default', gulp.series(reset, atomic, layout, component, utility))
 
 function atomic() {
 	return gulp
-		.src('./atomic/all.scss')
+		.src('./scss/atomic/all.scss')
 		.pipe(sass(sassOption).on('error', sass.logError))
 		.pipe(autoprefixer({ overrideBrowserslist: ['last 2 versions'] }))
 		.pipe(
@@ -28,7 +28,7 @@ function atomic() {
 
 function layout() {
 	return gulp
-		.src('./layout/all.scss')
+		.src('./scss/layout/all.scss')
 		.pipe(sass(sassOption).on('error', sass.logError))
 		.pipe(autoprefixer({ overrideBrowserslist: ['last 2 versions'] }))
 		.pipe(
@@ -43,7 +43,7 @@ function layout() {
 
 function reset() {
 	return gulp
-		.src('./reset.scss')
+		.src('./scss/reset.scss')
 		.pipe(sass(sassOption).on('error', sass.logError))
 		.pipe(autoprefixer({ overrideBrowserslist: ['last 2 versions'] }))
 		.pipe(
@@ -58,7 +58,7 @@ function reset() {
 
 function component() {
 	return gulp
-		.src('./component/all.scss')
+		.src('./scss/component/all.scss')
 		.pipe(sass(sassOption).on('error', sass.logError))
 		.pipe(autoprefixer({ overrideBrowserslist: ['last 2 versions'] }))
 		.pipe(
@@ -73,7 +73,7 @@ function component() {
 
 function utility() {
 	return gulp
-		.src('./utility/all.scss')
+		.src('./scss/utility/all.scss')
 		.pipe(sass(sassOption).on('error', sass.logError))
 		.pipe(autoprefixer({ overrideBrowserslist: ['last 2 versions'] }))
 		.pipe(
