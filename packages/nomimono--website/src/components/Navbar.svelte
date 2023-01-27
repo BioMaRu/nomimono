5192b41a1a3010be35096ce0550d9b3bfc841a6d
BioMaRu <Bio.color.palette@gmail.com>
2023-01-28 02:01:38 +0700

    feat: update document

--- packages/nomimono--website/src/components/Navbar.svelte
+++ packages/nomimono--website/src/components/Navbar.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import ThemeDropdown from "./ThemeDropdown.svelte";
+	import ThemeDropdown from './ThemeDropdown.svelte'
 </script>
 
 <nav class="navbar _bgcl-base-100 _dp-f _alit-ct _pdh-7 _pst-f _t-0 _w-100pct _zid-2">
@@ -12,36 +12,40 @@
 			<div tabindex="0">
 				<span class="nm-button is-variant-ghost">Getting Start</span>
 			</div>
-			<ul class="nm-menu _bsd-100 _bdw-1 _bdcl-content _bdcl-opacity-15">
+			<ul class="nm-menu _bsd-100 _bdw-1 _bdcl-content _bdcl-opacity-15 _bdrd-4">
 				<li>
-					<a href="/doc/introduction" sveltekit:prefetch>Introduction</a>
+					<a href="/doc/introduction">Introduction</a>
 				</li>
 				<li>
-					<a href="/doc/installation" sveltekit:prefetch>Installation</a>
+					<a href="/doc/installation">Installation</a>
 				</li>
 			</ul>
 		</div>
-		<div class="nm-dropdown is-hover-open">
+
+		<a href="/doc/introduction">
+			<span class="nm-button is-variant-ghost">Document</span>
+		</a>
+		<!-- <div class="nm-dropdown is-hover-open">
 			<div tabindex="0">
 				<span class="nm-button is-variant-ghost">Document</span>
 			</div>
 			<ul class="nm-menu _bsd-100 _bdw-1 _bdcl-content _bdcl-opacity-15">
 				<li>
-					<a href="/component/badge" sveltekit:prefetch>Components</a>
+					<a href="/component/badge">Components</a>
 				</li>
 			</ul>
-		</div>
+		</div> -->
 
-		<div class="nm-dropdown is-hover-open">
+		<!-- <div class="nm-dropdown is-hover-open">
 			<div tabindex="0">
 				<span class="nm-button is-variant-ghost">Framework</span>
 			</div>
 			<ul class="nm-menu _bsd-100 _bdw-1 _bdcl-content _bdcl-opacity-15">
 				<li>
-					<a href="/" sveltekit:prefetch>React component</a>
+					<a href="/">React component</a>
 				</li>
 			</ul>
-		</div>
+		</div> -->
 	</div>
 
 	<div class="_mgl-at">
--- packages/nomimono--website/src/routes/doc/introduction.svelte
+++ packages/nomimono--website/src/routes/doc/introduction.svelte
@@ -6,33 +6,41 @@
 <br />
 <br />
 <p>
-	<strong class="_cl-content">Hello and welcome!</strong>
+	<strong class="_cl-content _fs-6">Hello and welcome!</strong>
 </p>
 <p>
-	Welcome to the document of Nomimono (aka. 'a drink' in Japanese) UI framework. Here, you can mix
-	your colorful drink 🍸 like a bartender.
+	Welcome to the document of Nomimono. Inspired by the Japanese term for "a drink", Nomimono is a
+	user-friendly CSS framework that allows you to blend and customize the visual elements of your
+	website just like a bartender creates the perfect cocktail.
 </p>
 
 <section>
 	<h5 id="philosophy">Philosophy</h5>
 	<p>
-		Nomimono created to be simple, ready to use, and easy customize to build your own
-		maintainable design systems. Nomimono help you generate atomic class and battery included
-		with essential components.
+		Nomimono is designed to make web development simple, efficient, and enjoyable. With its
+		focus on ready-to-use, easy-to-customize elements, Nomimono helps you create maintainable
+		design systems that are tailored to your specific needs. Whether you're building a simple
+		landing page or a complex web application, Nomimono has you covered with a wide range of
+		atomic classes and essential components. With Nomimono, you can spend less time worrying
+		about the technical details and more time focusing on creating the perfect website for you
+		and your users.
 	</p>
 	<p>
-		Nomimono is built using SCSS and uses CSS custom property for configuring theme and
-		component. You can change the theme or switch dark and light modes just by swapping into the
-		different theme CSS files without a rebuild.
+		Nomimono is built using SCSS, which provides a powerful and flexible way to write and
+		organize your CSS. To make customization even easier, Nomimono uses CSS custom properties
+		for configuring the theme and components. This means that you can change the theme or switch
+		between dark and light modes by simply swapping in different theme CSS files, without the
+		need for a rebuild. This allows you to quickly and easily experiment with different looks
+		and styles, and ensures that your website stays looking fresh and modern no matter what
+		changes you make.
 	</p>
-	<p>Nomimono also help you get start for organize your design system too.</p>
 </section>
 
 <section>
 	<h5 id="overview">Overview</h5>
 	<p>
-		Out of the box, Nomimono provides you with a core
-		<a
+		Nomimono comes packed with everything you need to start building your website right out of
+		the box. The core package includes <a
 			class="nm-link _tdcrt-udl"
 			href="https://www.npmjs.com/package/@nomimono/nomimono-css"
 			target={'_blank'}>@nomimono/nomimono-css</a
@@ -42,8 +50,10 @@
 			class="nm-link _tdcrt-udl"
 			href="https://www.npmjs.com/package/@nomimono/nomimono-theme"
 			target={'_blank'}>@nomimono/nomimono-theme</a
-		> Which is already ready to import and use. you can use default light.css / dark.css theme or
-		create you your theme.
+		>, which are ready to be imported and used immediately. The default light and dark themes
+		are included, and you can also create your own custom theme with ease. With Nomimono, you
+		can have your website up and running in no time and have the flexibility to change the look
+		and feel of your website as per your requirements.
 	</p>
 </section>
 
